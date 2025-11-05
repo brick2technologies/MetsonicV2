2279c3b5d0ac4f9741bda7deb0dad46c48b8fb0b
karthikch2630 <karthikchitti55@gmail.com>
2025-11-05 12:52:58 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Products from "./pages/products";
+import Products from "./pages/Product";
 import Contact from "./pages/Contact";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
